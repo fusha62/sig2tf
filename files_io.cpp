2eb93cd206cb4253f69939090f92b4a90fa8b6d5
Tomohiro Minagawa <fusha01@hotmail.com>
2013-08-11 17:11:20 +0900

    ファイルへの書き出しを実装した．

--- files_io.cpp
+++ files_io.cpp
@@ -5,6 +5,7 @@
 int main(){
   // open input file
   SndfileHandle infile("./a.wav",SFM_READ);
+  if (not infile) return -1;
   
   // print file infomation
   std::cout << infile.format() << std::endl;
@@ -14,10 +15,19 @@ int main(){
   // read frames
   std::vector<short int> input(infile.frames());
   infile.readf(input.data(), infile.frames());
+
   // print input 
-   for (int i = 0; i < input.size(); ++i) {
-     std::cout << ' ' << input[i] << std::endl;
-   }
+   // for (int i = 0; i < input.size(); ++i) {
+   //   std::cout << ' ' << input[i] << std::endl;
+   // }
+   
+   // write output file
+   const char* outfilename="foo.wav";
+   SndfileHandle outfile(outfilename, SFM_WRITE,
+			 infile.format(), infile.channels(), infile.samplerate());
+   if (not outfile) return -1;
+   outfile.write(input.data(), input.size());
+   
 
   return 0;
 }
